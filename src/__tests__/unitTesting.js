61d7c41d6be75bd8d402544f527df3e272a59cf4
musiclover121 <musiclover11221@gmail.com>
2024-03-21 14:01:29 -0700

    Fixed generateChartImg test
    new dependancy: whatwg-fetch
    cleaned up comments

--- src/__tests__/unitTesting.js
+++ src/__tests__/unitTesting.js
@@ -1,44 +1,16 @@
 /**
-* @jest-environment jsdom
+* @jest-environment ./src/fixjsdomenvironment.js
 */
-
-// test('...', function() {
-//     expect('...').toBe('...')
-// })
-
-//Notes:
-//  my tests run calling functions, integration runs clicking buttons.
-//  need webpage "built" to interact with elements, but function calls made, not clicked
-//  should still look for results in user-like way, b/c better for changing page
-//      write any assumptions/dependencies above each test/suite
-//  check that require called correctly...???
 
 const { saveChart, loadAllSavedCharts, loadSavedChart, updateCurrentChartData,
     loadCurrentChartData } = require('../lib/chartStorage.js')
-//const chartStorage = require('../src/lib/chartStorage.js')
-//need chartBuilder?
 const generateChartImg = require('../lib/generateChartImg.js')
 const sortPoints = require('../lib/sortPoints.js')  //correct formatting for single function
-//add jsdom and stuff???
-const fs = require("fs")  //needed for initDomFromFiles()
-const domTesting = require('@testing-library/dom')
-require('@testing-library/jest-dom') //don't need to store it b/c only augmenting
-// const userEvent = require("@testing-library/user-event").default
-// function initDomFromFiles(htmlPath, jsPath) {    //may have multiple .js documents? //do they need to be isolated?
-//     const html = fs.readFileSync(htmlPath, 'utf8')
-//     //load html, but not always script file(s)
-//     document.open()
-//     document.write(html)
-//     document.close()
-//     //when seperate script file(s), use this to load test script without caching vars
-//     jest.isolateModules(function() {
-//         require(jsPath)
-//     })
-// }
 
-//dirname ensures grabbing test's directory("./")
-//initDomFromFiles(`${__dirname}/../registerUser.html`, `${__dirname}/../registerUser.js`)
+require('@testing-library/jest-dom')  //augmentation for window.local storage
+require("whatwg-fetch")  //augmentation for api fetch call
 
+//@testing-library/jest-dom and chartStorage.js dependancies
 describe('chartStorage.js Unit Tests', () => {
     //saveChart (doesn't care about incomplete charts)
     test('saveChart function saves chart with 3 datapoints, name, and color', () => {
@@ -119,7 +91,6 @@ describe('chartStorage.js Unit Tests', () => {
         expect(JSON.parse(window.localStorage.getItem("savedCharts")).length).toBe(1)
         expect(JSON.parse(window.localStorage.getItem("savedCharts"))[0]).toMatchObject(newChart)
     })
-    //one more test for idx > charts.length?
 
     //loadAllSavedCharts
     test('loadAllSavedCharts returns an empty array when no charts are saved', () => {
@@ -251,11 +222,8 @@ describe('chartStorage.js Unit Tests', () => {
         //Assert
         expect(loadSavedChart(1)).toMatchObject(chartList[1])  //need to make that an empty array?
     })
-    //could add border cases?
 
     //updateCurrentChartData
-        //only that it saves?
-        //could do cases that are missing data, but idk that's necessary?
     test('updateCurrentChartData saves the given chart in local storage', () => {
         //Assert
         window.localStorage.clear()
@@ -282,8 +250,6 @@ describe('chartStorage.js Unit Tests', () => {
     })
     
     //loadCurrentChartData
-        //only that it loads?
-        //could do cases that are missing data, but idk that's necessary?
     test('loadCurrentChartData loads the expected chart from local storage', () => {
         //Assert
         window.localStorage.clear()
@@ -307,32 +273,23 @@ describe('chartStorage.js Unit Tests', () => {
     })
 })
 
-// describe('generateChartImg.js Unit Tests', () => {
-//      //add generate chart image tests here...
-//      //can't test url match previous tests (changes with each function call)
-//      //try matching image to previous image (pixel for pixel)?
-//      //or just test that it's a url?
-//      //failure returns null or undefined?
-//      test('generateChartImg function generates an image with valid input', () => {
-//         //set inputs
-//         let pointsArray = [
-//             {x: 1, y: 2},
-//             {x: 5, y: 9},
-//             {x: 6, y: 10},
-//             {x: 7, y: 11}
-//         ];
-//         //doesn't like the function call, issues with fetch not defined?
-//         expect(generateChartImg("line", pointsArray, "Time", "Temperature", "Summer Temperatures, 2023", "blue")).not.toBe(undefined)
-//      })
-// })
+//generateChartImg.js, fixjsdomenvironment.js, and whatwg-fetch dependancies
+describe('generateChartImg.js Unit Tests', () => {
+    //following test written by Ellie
+    test("generateChartImg() returns a URL to a chart", async function() {
+        // arrange     
+        const coords = [{x: 1, y: 2}, {x: 3, y: 4}, {x: 5, y: 6}]
+        // act     
+        const url = await generateChartImg("line", coords, "x-axis", "y-axis", "title", "#ffffff")      
+        // assert     
+        expect(url).toContain("blob:nodedata:")
+    })
+})
 
 //sortPoints.js dependency
 describe('sortPoints.js Unit Tests', () => {
-    //notes:
-    //  need to format as json? or can just supply list of points? (array of {x:..., y:...} datapoints)
-    //  ready for testing?
 
-    //add sort points test here...
+    //sortPoints
     test('Sort points [(6, 10), (1,2), (7,11), (5,9)] to be [(1,2), (5,9), (6, 10), (7,11)]', () => {
         let givenPointsArray = [
             {x: 6, y: 10},
